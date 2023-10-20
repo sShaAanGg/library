fbc9c9e34191e6982d261b6b275223f47b004473
Tom Tseng <sshaaangg.tw@gmail.com>
2023-10-20 17:25:11 +0800

    fix&test: set headless=True
    **IMPORTANT NOTE!!!**
    Must be careful to set
    ```
    browser = playwright.chromium.launch(headless=True)
    ```
    the headless setting to True

---
+++ source/test/system-test/test_login.py
@@ -0,0 +1,23 @@
+import login
+import database
+# import pytest
+
+def test_login():
+    # Arrange
+    # Connect to the database
+    cnx = database.connect_mysql()
+    assert cnx is not None, "Connection is OK"
+    # Create a cursor object, which is used to execute SQL statements
+    cursor = cnx.cursor()
+
+    # Act
+    login.sync_playwright().start()
+
+    # Assert
+    # Check if the account is created
+    cursor.execute("USE library")
+    cursor.execute("SELECT * FROM account")
+    accounts = cursor.fetchall()
+    # TODO: add assertions
+    cursor.close()
+    cnx.close()
